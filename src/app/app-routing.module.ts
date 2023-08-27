28c6fde22fe6f18d2327f23969102d3020de8c33
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-27 01:29:57 -0400

    Create Student List Table on student page, with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 import { StudentCreateComponent } from './Pages/student-create/student-create.component';
+import { StudentPageComponent } from './Pages/student-page/student-page.component';
 
 // Routing for each component.
 const routes: Routes = [
   {path:'',component: HomePageComponent, title: 'Home Page' },
   {path:'about-us', component: AboutPageComponent, title: 'About Us' },
   {path:'contact-us',component: ContactPageComponent, title: 'Contact Us' },
+  {path:'students',component: StudentPageComponent, title: 'Student List' },
   {path:'student/create',component: StudentCreateComponent, title: 'Student Create' }
 ];
 
