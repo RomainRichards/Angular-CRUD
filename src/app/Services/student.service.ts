224fc6ed6cd3ce27d4c9628a2a9e2a0029f7f475
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-26 20:28:22 -0400

    Added Http Request

--- src/app/Services/student.service.ts
+++ src/app/Services/student.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// Http call request
 export class StudentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  saveStudent(input: object){
+        
+    return this.httpClient.post(''); // post method to save the data.
+  }
 }
